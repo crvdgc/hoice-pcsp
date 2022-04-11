f123d09c614f49f2ffce4e51412c942316e5ff86
crvdgc <ubikium@gmail.com>
2022-04-11 15:08:07 +0900

    add nix format

--- flake.nix
+++ flake.nix
@@ -11,24 +11,23 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlay = self: _: {
-          hsPkgs =
-            self.haskell-nix.project' rec {
-              src = ./.;
-              compiler-nix-name = "ghc8107";
-              shell = {
-                tools = {
-                  cabal = "latest";
-                  # ghcid = "latest";
-                  haskell-language-server = "latest";
-                  # hlint = "latest";
-                  # See https://github.com/input-output-hk/haskell.nix/issues/1337
-                  ormolu = {
-                    version = "latest";
-                    modules = [ ({ lib, ... }: { options.nonReinstallablePkgs = lib.mkOption { apply = lib.remove "Cabal"; }; }) ];
-                  };
+          hsPkgs = self.haskell-nix.project' rec {
+            src = ./.;
+            compiler-nix-name = "ghc8107";
+            shell = {
+              tools = {
+                cabal = "latest";
+                # ghcid = "latest";
+                haskell-language-server = "latest";
+                # hlint = "latest";
+                # See https://github.com/input-output-hk/haskell.nix/issues/1337
+                ormolu = {
+                  version = "latest";
+                  modules = [ ({ lib, ... }: { options.nonReinstallablePkgs = lib.mkOption { apply = lib.remove "Cabal"; }; }) ];
                 };
               };
             };
+          };
         };
         pkgs = import nixpkgs {
           inherit system;
@@ -39,6 +38,11 @@
         };
         flake = pkgs.hsPkgs.flake { };
       in
-      flake // { defaultPackage = flake.packages."hoice-pcsp:exe:hoice-pcsp"; }
+      flake // {
+        defaultPackage = flake.packages."hoice-pcsp:exe:hoice-pcsp";
+        devShell = flake.devShell.overrideAttrs (oldAttrs: {
+          buildInputs = (oldAttrs.buildInputs or [ ]) ++ [ pkgs.nixpkgs-fmt ];
+        });
+      }
     );
 }
